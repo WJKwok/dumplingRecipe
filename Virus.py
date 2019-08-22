c31843b0418757b232f53dcee4a1d2a94c3be829
Victor Bellu <v.c.bellu@gmail.com>
2019-08-22 12:39:07 +0200

    *evil laugh*

---
+++ Virus.py
@@ -0,0 +1,8 @@
+
+import time
+
+print("I'm a virus but I'm not developed yet.")
+time.sleep(0.5)
+print("Could you please do me a favor and just delete something important yourself?")
+time.sleep(0.5)
+print("Thanks.")
